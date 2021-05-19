bb70f53d54cde2fd85f3da726312beb7d4584c77
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-20 02:12:23 +0530

    Added:class Email.java

--- regex.java
+++ regex.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Email {
-	public static boolean isValidPinCode(String email) {
+	public static boolean isValidEmail(String email) {
 		
        String regex = "^[a-z]{3}[.][a-z]{3}+[@][a-z]{5,10}[.][a-z]{2}[.][a-z]{2}$" ; 
       
@@ -19,7 +19,7 @@ public class Email {
     } public static void main(String args[]) {
         // Start Test Case 1:
         String email = "abc.xyz@bridgelabz.co.in";
-        System.out.println( email + " : " + isValidPinCode(email));
+        System.out.println( email + " : " + isValidEmail(email));
     
     }
 }
