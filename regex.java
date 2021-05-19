34b62df6a7fe1ece4502821cb696afcae7dde626
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-20 02:03:09 +0530

    Added:class Email.java

--- regex.java
+++ regex.java
@@ -3,40 +3,23 @@ package pincode;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Pincode {
-
-	 // Function to validate 
-    public static boolean isValidPinCode(String pinCode)
-    {
-        String regex = "^[1-9]{1}[0-9]{2}\s{0,1}[0-9]{3}$"; 
-        Pattern p = Pattern.compile(regex); 
-        if (pinCode == null) {
+public class Email {
+	public static boolean isValidPinCode(String email) {
+		
+       String regex = "^[a-z]{3}[.][a-z]{3}+[@][a-z]{5,10}[.][a-z]{2}[.][a-z]{2}$" ; 
+      
+       Pattern p = Pattern.compile(regex); 
+        if (email == null) {
             return false;
         }
   
-        Matcher m = p.matcher(pinCode);
-        return m.matches();
-    }
-  
-    public static void main(String args[]) {
+        Matcher m = p.matcher(email);
+        return true;
+        
+    } public static void main(String args[]) {
         // Start Test Case 1:
-        String num1 = "400088";
-        System.out.println( num1 + ": " + isValidPinCode(num1));
-  
-        // Start Test Case 2:
-        String num2 = "A400088";
-        System.out.println( num2 + ": " + isValidPinCode(num2));
-  
-        // Start Test Case 3:
-        String num3 = "400088B";
-        System.out.println( num3 + ": " + isValidPinCode(num3));
-  
-        // Start Test Case 4:
-        String num4 = "400 088";
-        System.out.println(num4 + ": " + isValidPinCode(num4));
+        String email = "abc.xyz@bridgelabz.co.in";
+        System.out.println( email + " : " + isValidPinCode(email));
+    
     }
 }
-	
-	
-	
-	
